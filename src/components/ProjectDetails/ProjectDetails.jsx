87e698fafd4cbddd801367520d04e9b9632798e7
panditharshpandeyji <harsh4105pandey@gmail.com>
2024-12-07 01:11:30 +0530

    Update Add More Projects ProjectDetails.jsx

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -5,6 +5,22 @@ const ProjectList = () => {
   // Example data for projects
   const projects = [
     {
+      title: "EM Accounts Live Project",
+      description:
+        "This application focuses on society management and offers dynamic features for efficient operations. I worked on every aspect of this project",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJP-fgszMpL0NrgOhxfbrHqf9NcLgO9xCb9w&s",
+      projectUrl: "http://13.202.179.147/SocietyMaintenance",
+    },
+    {
+      title: "College Bus Schedule Application",
+      description:
+        "I developed a College Bus Schedule Application aimed at simplifying transportation for students.",
+      imageUrl:
+        "https://t3.ftcdn.net/jpg/05/71/69/10/360_F_571691018_GxAIRdpQ1wk38db2lYkWQEhxqalnBsL3.jpg",
+      projectUrl: "",
+    },
+    {
       title: "Real Time Chat App",
       description:
         "Developed a dynamic real-time chat application using the MERN stack (MongoDB, Express.js, React, Node.js) and Socket.IO. The application supports instant messaging with features like user authentication, private and group chats.",
